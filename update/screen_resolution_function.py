6f8089245d21f35cfacca5ebccc55e3579bb76fe
Kriss <geraltrivia19@gmail.com>
2022-06-08 15:42:26 +0200

    Completed and tested calculating_the_middle_position_on_screen and small cosmetic changes.

--- update/screen_resolution_function.py
+++ update/screen_resolution_function.py
@@ -1,9 +1,10 @@
-from update.all_setting import MINIMAL_SUPPORTED_RESOLUTION_WIDTH_HEIGHT, MAIN_WINDOW_RESOLUTION
+from update.constant import MINIMAL_SUPPORTED_RESOLUTION_WIDTH_HEIGHT, MAIN_WINDOW_RESOLUTION
 
 screen_resolution_width, screen_resolution_height = None, None
+horizontal_position_on_screen, vertical_position_on_screen = None, None
 
 
-def save_width_and_height_screen_resolution(root_tkinter_tk):
+def get_and_save_screen_resolution(root_tkinter_tk):
     global screen_resolution_width, screen_resolution_height
     if not (screen_resolution_width and screen_resolution_height):
         screen_resolution_width = root_tkinter_tk.winfo_screenwidth()
@@ -23,6 +24,6 @@ def check_the_minimum_resolution_requirements():
 
 
 def calculating_the_middle_position_on_screen():
+    global horizontal_position_on_screen, vertical_position_on_screen
     horizontal_position_on_screen = -(screen_resolution_width // -2) + (MAIN_WINDOW_RESOLUTION[0] // -2)
     vertical_position_on_screen = -(screen_resolution_height // -2) + (MAIN_WINDOW_RESOLUTION[1] // -2)
-    return horizontal_position_on_screen, vertical_position_on_screen
--- update/tests/test_screen_resolution_function.py
+++ update/tests/test_screen_resolution_function.py
@@ -1,22 +1,10 @@
 import pytest
-from tkinter import Tk
 from unittest.mock import Mock
 
-import update.screen_resolution_function
+import update.screen_resolution_function as srf
 from update.screen_resolution_function import \
     return_screen_resolution_width_height, check_the_minimum_resolution_requirements, \
-    save_width_and_height_screen_resolution, calculating_the_middle_position_on_screen
-
-
-@pytest.fixture
-def tkinter_tk():
-    return Tk()
-
-
-@pytest.fixture(name="resolution_to_none")
-def setting_the_saved_screen_resolution_to_none():
-    update.screen_resolution_function.screen_resolution_width = None
-    update.screen_resolution_function.screen_resolution_height = None
+    get_and_save_screen_resolution, calculating_the_middle_position_on_screen
 
 
 @pytest.fixture
@@ -26,19 +14,25 @@ def mock_screen_resolution(tkinter_tk):
     return tkinter_tk.winfo_screenwidth, tkinter_tk.winfo_screenheight
 
 
+@pytest.fixture
+def set_resolution_for_none():
+    srf.screen_resolution_width = None
+    srf.screen_resolution_height = None
+
+
 @pytest.mark.parametrize("glob_screen_width,glob_screen_height,expected", (
         (1280, 720, (1280, 720)),
         (1600, 900, (1600, 900)),
 
 ))
-def test_save_width_and_height_screen_resolution_if_is_true(resolution_to_none, tkinter_tk, mock_screen_resolution,
-                                                            glob_screen_width, glob_screen_height, expected):
+def test_save_width_and_height_screen_resolution_if_is_true(set_resolution_for_none, tkinter_tk, mock_screen_resolution,
+                                                            glob_screen_width,
+                                                            glob_screen_height, expected):
     mock_screen_resolution[0].return_value = glob_screen_width
     mock_screen_resolution[1].return_value = glob_screen_height
 
-    save_width_and_height_screen_resolution(tkinter_tk)
-    result = update.screen_resolution_function.screen_resolution_width, \
-             update.screen_resolution_function.screen_resolution_height
+    get_and_save_screen_resolution(tkinter_tk)
+    result = srf.screen_resolution_width, srf.screen_resolution_height
 
     assert result == expected
 
@@ -51,42 +45,39 @@ def test_save_width_and_height_screen_resolution_if_is_true(resolution_to_none, 
 def test_save_width_and_height_screen_resolution_if_is_false(tkinter_tk, mock_screen_resolution, glob_screen_width,
                                                              glob_screen_height, new_value_screen_width,
                                                              new_value_screen_height, expected):
-    update.screen_resolution_function.screen_resolution_width = glob_screen_width
-    update.screen_resolution_function.screen_resolution_height = glob_screen_height
+    srf.screen_resolution_width = glob_screen_width
+    srf.screen_resolution_height = glob_screen_height
     mock_screen_resolution[0].return_value = new_value_screen_width
     mock_screen_resolution[1].return_value = new_value_screen_height
 
-    save_width_and_height_screen_resolution(tkinter_tk)
-    result = update.screen_resolution_function.screen_resolution_width, \
-             update.screen_resolution_function.screen_resolution_height
+    get_and_save_screen_resolution(tkinter_tk)
+    result = srf.screen_resolution_width, srf.screen_resolution_height
 
     assert result == expected
 
 
-@pytest.mark.parametrize("tmp_width_resolution,tmp_height_resolution", (
+@pytest.mark.parametrize("tmp_width,tmp_height", (
         (1280, 720),
         (1279, 720),
         (1280, 719),
 ))
-def test_return_screen_resolution_width_height(resolution_to_none, mock_screen_resolution, tkinter_tk,
-                                               tmp_width_resolution,
-                                               tmp_height_resolution):
-    mock_screen_resolution[0].return_value = tmp_width_resolution
-    mock_screen_resolution[1].return_value = tmp_height_resolution
-    save_width_and_height_screen_resolution(tkinter_tk)
+def test_return_screen_resolution_width_height(set_resolution_for_none, mock_screen_resolution, tkinter_tk, tmp_width,
+                                               tmp_height):
+    mock_screen_resolution[0].return_value = tmp_width
+    mock_screen_resolution[1].return_value = tmp_height
+    get_and_save_screen_resolution(tkinter_tk)
 
-    assert return_screen_resolution_width_height() == (tmp_width_resolution, tmp_height_resolution)
+    assert return_screen_resolution_width_height() == (tmp_width, tmp_height)
 
 
-@pytest.mark.parametrize("tmp_width_resolution,tmp_height_resolution,expected", (
+@pytest.mark.parametrize("tmp_width,tmp_height,expected", (
         (1280, 720, True),
         (1279, 720, False),
         (1280, 719, False),
 ))
-def test_check_the_minimum_resolution_requirements(mock_screen_resolution, tmp_width_resolution, tmp_height_resolution,
-                                                   expected):
-    mock_screen_resolution[0].return_value = tmp_width_resolution
-    mock_screen_resolution[1].return_value = tmp_height_resolution
+def test_check_the_minimum_resolution_requirements(tmp_width, tmp_height, expected):
+    srf.screen_resolution_width = tmp_width
+    srf.screen_resolution_height = tmp_height
 
     assert check_the_minimum_resolution_requirements() == expected
 
@@ -97,10 +88,11 @@ def test_check_the_minimum_resolution_requirements(mock_screen_resolution, tmp_w
         (1920, 1080, (360, 240)),
         (3840, 2160, (1320, 780)),
 ))
-def test_calculating_the_middle_position_on_screen(resolution_to_none, tmp_width_resolution,
-                                                   tmp_height_resolution, expected):
-    update.screen_resolution_function.screen_resolution_width = tmp_width_resolution
-    update.screen_resolution_function.screen_resolution_height = tmp_height_resolution
+def test_calculating_the_middle_position_on_screen(tmp_width_resolution, tmp_height_resolution,
+                                                   expected):
+    srf.screen_resolution_width = tmp_width_resolution
+    srf.screen_resolution_height = tmp_height_resolution
+    calculating_the_middle_position_on_screen()
 
-    result = calculating_the_middle_position_on_screen()
+    result = srf.horizontal_position_on_screen, srf.vertical_position_on_screen
     assert result == expected
